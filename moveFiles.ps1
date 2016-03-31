c68d7c479a9cb1ae4d4f44c36a4c7b53476f2956
Peter Gerrard <peter.gerrard@red-gate.com>
2016-03-31 11:24:41 +0100

    Remove pdb files that bloat the extension size

--- moveFiles.ps1
+++ moveFiles.ps1
@@ -8,10 +8,14 @@ $sqlDataCompareDirectory     = $env:DLMAS_HOME + 'SDC\'
 $sqlCompareDirectory         = $env:DLMAS_HOME + 'SC\'
 $sqlReleaseModulesDirectory  = $env:DLMAS_HOME + 'Modules\'
 
-Remove-Item .\TheTask\SDC      -Recurse
-Remove-Item .\TheTask\SC       -Recurse
-Remove-Item .\TheTask\Modules  -Recurse
+@(".\TheTask\SDC", ".\TheTask\SC", ".\TheTask\Modules") | % {
+    if (Test-Path $_) {
+        Remove-Item $_ -Recurse
+    }
+}
 
 Copy-Item $sqlCompareDirectory         .\TheTask\SC        -recurse -force
 Copy-Item $sqlDataCompareDirectory     .\TheTask\SDC       -recurse -force
 Copy-Item $sqlReleaseModulesDirectory  .\TheTask\Modules   -recurse -force
+
+Get-ChildItem .\TheTask -Recurse -Filter *.pdb -File | Remove-Item
